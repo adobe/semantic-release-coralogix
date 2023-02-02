3ab0da1c0a2c66884f782098c9df55a1260278f3
Dominique Pfister <dpfister@adobe.com>
2023-02-02 16:05:50 +0100

    fix: clean up fetch context

--- src/verify.js
+++ src/verify.js
@@ -10,16 +10,21 @@
  * governing permissions and limitations under the License.
  */
 
-const { verifytoken } = require('./coralogix-tagger.js');
+const { verifytoken, reset } = require('./coralogix-tagger.js');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = async function verify(pluginConfig, { logger }) {
   if (!process.env.CORALOGIX_TAGGER_API_KEY) {
     throw new Error('Environment variable CORALOGIX_TAGGER_API_KEY is not set, unable to tag releases in Coralogix');
   }
-  await verifytoken({
-    ...pluginConfig,
-    API_KEY: process.env.CORALOGIX_TAGGER_API_KEY,
-  });
-  logger.log('Coralogix Token is valid, releases can be tagged in Coralogix');
-  return true;
+  try {
+    await verifytoken({
+      ...pluginConfig,
+      API_KEY: process.env.CORALOGIX_TAGGER_API_KEY,
+    });
+    logger.log('Coralogix Token is valid, releases can be tagged in Coralogix');
+    return true;
+  } finally {
+    await reset();
+  }
 };
